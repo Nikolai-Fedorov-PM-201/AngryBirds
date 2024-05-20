38c89038908ef164ba818cc5cd44741ddb8f022d
Nikolai-Fedorov-PM-201 <146085274+Nikolai-Fedorov-PM-201@users.noreply.github.com>
2024-05-20 22:14:14 +0500

    Add files via upload

---
+++ AngryBirdsProject-master/MainWindow.xaml.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+
+
+
+namespace AngryBirdsProject
+{
+
+    public partial class MainWindow : Window
+    {
+        public Bird ptichka;
+        public Barrier beam;
+     
+
+        public MainWindow(Bird bird, Barrier barrier)
+        {
+            InitializeComponent();
+            this.ptichka = bird;
+            this.beam = barrier;
+            Rectangle rectangle = new Rectangle
+            {
+                Stroke = Brushes.Black,
+                Fill = Brushes.Red,
+                Width = 30,  // желаемая ширина фигуры
+                Height = beam.height  // желаемая высота фигуры
+            };
+
+            // Добавьте прямоугольник на canvas
+            MainCanvas.Children.Add(rectangle);
+
+            // Задайте расположение прямоугольника
+            Canvas.SetLeft(rectangle, beam.distance+36);
+            Canvas.SetTop(rectangle, 600-beam.height-80);
+
+        }
+        private async Task PerformTaskWithDelay()
+        {
+            // Действие 1
+            // ... ваш код здесь ...
+            Storyboard shakeAnimation = this.Resources["ShakeAnimation"] as Storyboard;
+
+            // Установите элемент, к которому будет применяться анимация, как цель
+            Storyboard.SetTarget(shakeAnimation, this);
+
+            // Запустите анимацию
+            shakeAnimation.Begin();
+
+            // Ожидание задержки в 2 секунды
+            await Task.Delay(1000);
+
+            // Действие 2
+            // ... ваш код здесь ...
+            Finally gameover = new Finally();
+            gameover.Show();
+        }
+
+
+        DispatcherTimer timer = new DispatcherTimer();
+        int k = 0;
+        private void Bird_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            
+
+            // создаем таймер
+            
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 30);
+            timer.Tick += timer_tick;
+            
+
+            ptichka.Calculate(36, 172, beam);
+            ptichka.Write("file.txt");
+            
+            
+            timer.Start();
+
+
+
+        }
+      
+        private async void timer_tick(object sender, object e)
+        {
+            int i = ptichka.Points.Count;
+            if (ptichka.Points[k].x != -1)
+            {
+                Canvas.SetTop(red_bird, (600 - (ptichka.Points[k].y)));
+                Canvas.SetLeft(red_bird, 36 + (ptichka.Points[k].x));
+            } 
+            else
+            {
+                timer.Stop();
+
+                await PerformTaskWithDelay();
+
+                // MessageBox.Show("Произошло столкновение", "Конец", MessageBoxButton.OK);
+
+
+            }
+            
+            k++;
+            if (k== i) {
+                
+                timer.Stop(); }
+
+        }
+
+        
+           
+        
+    }
+}
+
+
+    
+
+    
+            
+
+
+        
+ 
+
+    
+
---
+++ AngryBirdsProject-master/Start_Window.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Xml.Serialization;
+
+
+namespace AngryBirdsProject
+{
+    
+    public partial class Start_Window
+    {
+       private void Button_Start_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            
+        
+            string def_velocity = start_velocity.Text;
+            def_velocity=string.IsNullOrWhiteSpace(def_velocity) ? "80" : def_velocity;
+            string def_corner = Corner.Text;
+            def_corner = string.IsNullOrWhiteSpace(def_corner) ? "30" : def_corner;
+            string def_mass = Mass.Text;
+            def_mass = string.IsNullOrWhiteSpace(def_mass) ? "5" : def_mass;
+            string def_x=Barrier_x.Text;
+            def_x = string.IsNullOrEmpty(def_x) ? "250" : def_x;
+            string def_y=Barrier_y.Text;
+            def_y=string.IsNullOrEmpty(def_y) ? "300" : def_y;
+
+            double velocity = Convert.ToDouble(def_velocity);
+            double corner = Convert.ToDouble(def_corner);
+            double mass = Convert.ToDouble(def_mass);
+            double x = Convert.ToDouble(def_x);
+            double y = Convert.ToDouble(def_y);
+            Bird bird = new Bird(velocity, corner, mass);
+            Barrier barrier = new Barrier(x, y);
+
+           
+
+            MainWindow mainWindow = new MainWindow(bird, barrier);
+            mainWindow.Show();
+
+        
+           
+
+         }
+    }
+}
